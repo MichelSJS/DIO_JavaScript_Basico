ad1430d211467467e634f118385832504d33398f
MichelSJS <michelsjs.12@gmail.com>
2021-07-26 17:07:10 -0300

    condicionais, loops e Date

--- js/main.js
+++ js/main.js
@@ -1,6 +1,38 @@
+var data = new Date();
+alert("Verifique o console para visualizar informações sobre datas");
+console.log(data.getUTCDate());
+console.log(data.getMonth()+1);
+console.log(data.getDay());
+
+/*
+var count;
+for(count = 0; count < 5; count++) {
+    console.log(count);
+}
+*/
+
+/*
+var count = 0;
+while (count <= 5) {
+    console.log(count);
+    count++
+}
+*/
+
+/*
+var idade = prompt("Qual a sua idade?");
+if (idade >= 18) {
+    alert("Maior de idade");
+} else {
+    alert("Menor de idade")
+}
+*/
+
+/*
 var frutas = [{nome: "maçã", cor:"vermelha"}, {nome:"uva", cor:"roxo"}]
 console.log(fruta);
 alert(fruta[1].nome);
+*/
 
 /*
 var fruta = {nome: "maçã", cor: "vermelha"}
