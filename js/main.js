08d56683cd35f47a23d16282222fac4904ae54a3
MichelSJS <michelsjs.12@gmail.com>
2021-07-26 11:56:05 -0300

    exemplo do uso de arrays e dicionários

--- js/main.js
+++ js/main.js
@@ -1,11 +1,34 @@
+var frutas = [{nome: "maçã", cor:"vermelha"}, {nome:"uva", cor:"roxo"}]
+console.log(fruta);
+alert(fruta[1].nome);
 
+/*
+var fruta = {nome: "maçã", cor: "vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+/*
+var lista = ["maçã", "pêra", "laranja"];
+lista.push("uva");
+lista.pop();
+*/
+
+/*
+console.log(lista);
+console.log(lista.toString());
+console.log(lista.join(" / "))
+*/
+
+/*
 var nome = "Michel Santos"
 var n1 = 7;
 var n2 = 15;
 var frase = "Islândia é o melhor time do mundo"
-//alert(nome + " tem " + idade + " anos")
-//alert(idade + idade2);
+alert(nome + " tem " + idade + " anos")
+alert(idade + idade2);
 console.log(nome);
 console.log(n1 * n2);
 console.log(frase.toUpperCase());
-//console.log(frase.replace("Islândia", "Noruega"));
+console.log(frase.replace("Islândia", "Noruega"));
+*/
