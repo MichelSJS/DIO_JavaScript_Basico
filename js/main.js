40236a6454fdbcc990cadb08eecc2220ab03111c
MichelSJS <michelsjs.12@gmail.com>
2021-07-26 10:03:51 -0300

    aprendizado e utilização de códigos básicos e inicias dentro do funcionamento do JavaScript

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+var nome = "Michel Santos"
+var n1 = 7;
+var n2 = 15;
+var frase = "Islândia é o melhor time do mundo"
+//alert(nome + " tem " + idade + " anos")
+//alert(idade + idade2);
+console.log(nome);
+console.log(n1 * n2);
+console.log(frase.toUpperCase());
+//console.log(frase.replace("Islândia", "Noruega"));
